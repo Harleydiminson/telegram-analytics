0c9fef86ab0bb16e01c171a46ce462552a259bb6
Dima <harleydiminson@gmail.com>
2025-01-22 19:16:49 +0100

    init

---
+++ src/layouts/Header.jsx
@@ -0,0 +1,25 @@
+/**
+ *                                  TELEGRAM-ANALYTICS
+ */
+
+function Header() {
+    return (
+      <header className="header">
+        <nav className="tool-bar">
+          <ul>
+            <li><h1>ANALYTICS</h1></li>
+            <li>
+              Search
+              <input type="text" />
+            </li>
+          </ul>
+          <ul>
+            <li>About</li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+  
+  export default Header;
+  
